625d24560035559fba5851c0b1650ec7308a9b33
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-04-23 16:51:39 +0200

    Update

--- lib.js
+++ lib.js
@@ -1,15 +1,24 @@
 
+
+
+
+//logger informasjonen på onchange
+function loginput() {
+console.log(textbox.value);
+  }
+  
+
 // Skjekker alle sammen
-function allchecked(assert) {
-    return checklength(assert) &&
-    checkdots(assert) &&
-    checkyear(assert) &&
-    checkmonth(assert) &&
-    checkday(assert);
+function allchecked(dato) {
+    return checklength(dato) &&
+    checkdots(dato) &&
+    checkyear(dato) &&
+    checkmonth(dato) &&
+    checkday(dato);
   }
   
   // skjekker lengden
-  function checklength(assert) {
+  function checklength(dato) {
     var str = textbox.value;
     if (str.length == 10) {
       return true;
@@ -17,7 +26,7 @@ function allchecked(assert) {
     }
   
     // skjekker punktum
-  function checkdots(assert) {
+  function checkdots(dato) {
     var str = textbox.value;
     var dotdot = str.substring(2,3) + str.substring(5,6);
     if (dotdot = "..") {
@@ -26,7 +35,7 @@ function allchecked(assert) {
     }
   
     // skjekker året
-  function checkyear(assert) {
+  function checkyear(dato) {
   var str = textbox.value;
   var year = str.substring(6,10);
   console.log(year);
@@ -36,21 +45,52 @@ function allchecked(assert) {
   }
   
   // skjekker måneden
-  function checkmonth(assert) {
-    var mon = textbox.value;
-    var month = mon.substring(3,5);
+  function checkmonth(dato) {
+    var str = textbox.value;
+    var month = str.substring(3,5);
     console.log(month);
     if (month.length === 2 && month >= '01' && month <= '12') {
       return true;
     }
+  }
+  
+  // skjekker dagsdatoen
+  function checkday(dato) {
+    var str = textbox.value;
+    var day = str.substring(0,2);
+    var month = str.substring(3,5);
+    var year = str.substring(6,10);
+    var isLeapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+    console.log(day);
+    if (day.length === 2 && day >= '01' && day <= '28') {
+      return true;
     }
-    
-    // skjekker dagsdatoen
-    function checkday(assert) {
-      var da = textbox.value;
-      var day = da.substring(0,2);
-      console.log(day);
-      if (day.length === 2 && day >= '01' && day <= '28') {
-        return true;
-      }
-      }
+    else if (day == '29' && month == '02'  && isLeapYear == true){
+      return true;
+    }
+    else if(day == ('29') || ('30') && !month =='02'){
+      return true;
+    }
+    else if (day == '31' && (month == '01' || month == '03' || month == '05' || month == '07' || month == '08' || month == '10' || month == '12'));{
+      return true;
+    }
+    if (day > '31');{
+      return false
+    }
+  }
+      //skjekker om det er skuddår
+          
+      
+      
+      function checkday(dato) {
+          var day = date.substring(0, 2);
+          var month = date.substring(3, 5);
+   var year = date.substring(6);
+   var isFebruary = month == '02';
+   var isShortMonth = month == '04' || month ==  '06' || month ==  '09' || month ==  '11';
+   var isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+   return day >= '01' && day <= '28'
+     || day == '29' && (!isFebruary|| isLeapYear)
+     || day == '30' && (!isFebruary)
+     || day == '31' && !isFebruary && !isShortMonth;
+}
